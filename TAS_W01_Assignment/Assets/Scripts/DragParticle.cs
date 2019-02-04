1dc272a9a1df5d16ef1624302193fc10ee139b25
Yiren Wang <yw4062@nyu.edu>
2019-02-03 23:06:57 -0500

    Assignment Week 01

---
+++ TAS_W01_Assignment/Assets/Scripts/DragParticle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragParticle : MonoBehaviour
+{
+    private Vector3 particleScreenSpace;
+    private Vector3 particleWorldSpace;
+    private Vector3 mouseScreenSpace;
+
+
+    void OnMouseDrag()
+
+    {
+        particleScreenSpace = Camera.main.WorldToScreenPoint(transform.position);
+        mouseScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, particleScreenSpace.z);
+        particleWorldSpace = Camera.main.ScreenToWorldPoint(mouseScreenSpace);
+        transform.position = particleWorldSpace;
+    }
+
+}
